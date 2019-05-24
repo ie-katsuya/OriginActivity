69e3ac74884556a5e9471769b27eca1135bd9140
ie-katsuya <k-aoki@i-enter.co.jp>
2019-05-24 10:32:22 +0900

    オリジナルアプリ

--- app/src/main/java/com/example/originactivity/MainActivity.kt
+++ app/src/main/java/com/example/originactivity/MainActivity.kt
@@ -4,34 +4,61 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.HashMap
+import com.google.firebase.auth.FirebaseAuth
+
 
 class MainActivity : AppCompatActivity() , View.OnClickListener {
+
+    private lateinit var mDatabaseReference: DatabaseReference
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Rogin_button.setOnClickListener(this)
+        // ログイン済みのユーザーを取得する
+        val user = FirebaseAuth.getInstance().currentUser
+
+        // ログインしていなければログイン画面に遷移させる
+        if (user == null) {
+            Acount_button.setVisibility(View.INVISIBLE)
+            Login_button.setVisibility(View.VISIBLE)
+            Create_button.setVisibility(View.INVISIBLE)
+            TaskList_button.setVisibility(View.INVISIBLE)
+        }else{
+            Acount_button.setVisibility(View.VISIBLE)
+            Login_button.setVisibility(View.INVISIBLE)
+            Create_button.setVisibility(View.VISIBLE)
+            TaskList_button.setVisibility(View.VISIBLE)
+        }
+
+        Login_button.setOnClickListener(this)
         TaskList_button.setOnClickListener(this)
-        Create_buttun.setOnClickListener(this)
+        Create_button.setOnClickListener(this)
         Acount_button.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
-        if(v == Rogin_button){
+        if(v == Login_button){
             //ログイン画面に遷移
-            //val intent = Intent(this, RoginActivity::class.java)
-            //startActivity(intent)
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }else if(v == TaskList_button){
             //タスク管理画面に遷移
             //val intent = Intent(this, TaskListActivity::class.java)
             //startActivity(intent)
-        }else if(v == Create_buttun){
+        }else if(v == Create_button){
             //タスク作成画面に遷移
             //val intent = Intent(this, CreateActivity::class.java)
             //startActivity(intent)
-        }else{
+        }else if(v == Acount_button){
             //アカウント設定画面に遷移
             val intent = Intent(this, SettingActivity::class.java)
             startActivity(intent)
