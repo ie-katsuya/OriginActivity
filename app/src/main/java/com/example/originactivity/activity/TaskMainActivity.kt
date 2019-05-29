41b6632c4d51ddf8b89212afa8ac97966ea49609
ie-katsuya <k-aoki@i-enter.co.jp>
2019-05-29 14:13:33 +0900

    オリジナルアプリ ログイン画面　○ アカウント設定画面　○ タスク作成画面　○

--- app/src/main/java/com/example/originactivity/activity/TaskMainActivity.kt
+++ app/src/main/java/com/example/originactivity/activity/TaskMainActivity.kt
@@ -6,24 +6,37 @@ import android.os.Bundle
 import android.view.View
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ListView
 import com.example.originactivity.R
-import com.google.firebase.database.DatabaseReference
+import com.example.originactivity.adapter.TasklistAdapter
+import com.example.originactivity.entity.Task
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class TaskMainActivity : AppCompatActivity() , View.OnClickListener {
 
     private lateinit var mDatabaseReference: DatabaseReference
+    private lateinit var mListView: ListView
+    private lateinit var mTaskArrayList: ArrayList<Task>
+    private lateinit var mAdapter: TasklistAdapter
+
+    var dataBaseReference = FirebaseDatabase.getInstance().reference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Search_button.setOnClickListener(this)
+        setupListView()
+
+
 
         fab.setOnClickListener { view ->
+            //タスク作成画面に遷移
             val intent = Intent(this, TaskCreateActivity::class.java)
             startActivity(intent)
         }
+
+        Search_button.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -49,4 +62,87 @@ class TaskMainActivity : AppCompatActivity() , View.OnClickListener {
 
         return super.onOptionsItemSelected(item)
     }
+
+    private fun setupListView(){
+        // Firebase
+        mDatabaseReference = FirebaseDatabase.getInstance().reference
+
+        // ListViewの準備
+        mListView = this.findViewById(R.id.listView)
+        mAdapter = TasklistAdapter(this)
+        mTaskArrayList = ArrayList<Task>()
+        mAdapter.notifyDataSetChanged()
+
+        // 質問のリストをクリアしてから再度Adapterにセットし、AdapterをListViewにセットし直す
+        mTaskArrayList.clear()
+        mAdapter.setTaskArrayList(mTaskArrayList)
+        mListView.adapter = mAdapter
+
+        var isChildEventEnabled = false
+        val contentRef = dataBaseReference.child("contents")
+
+        contentRef.addChildEventListener(object: ChildEventListener{
+            override fun onCancelled(databaseError: DatabaseError) {
+                if (!isChildEventEnabled) {
+                    return
+                }
+            }
+
+            override fun onChildMoved(datasnapshot: DataSnapshot, s: String?) {
+                if (!isChildEventEnabled) {
+                    return
+                }
+            }
+
+            override fun onChildChanged(datasnapshot: DataSnapshot, s: String?) {
+                if (!isChildEventEnabled) {
+                    return
+                }
+                updateItem()
+            }
+
+            override fun onChildAdded(datasnapshot: DataSnapshot, s: String?) {
+                if (!isChildEventEnabled) {
+                    return
+                }
+                appendItem()
+            }
+
+            override fun onChildRemoved(datasnapshot: DataSnapshot) {
+                if (!isChildEventEnabled) {
+                    return
+                }
+            }
+        })
+
+        contentRef.addListenerForSingleValueEvent(
+            object: ValueEventListener{
+                override fun onCancelled(p0: DatabaseError) {
+
+                }
+
+                override fun onDataChange(p0: DataSnapshot) {
+                    val count = p0.childrenCount
+                    appendAllItem()
+                    contentRef.removeEventListener(this)
+                    isChildEventEnabled = true
+                }
+            }
+        )
+
+    }
+
+    private fun appendAllItem() {
+
+    }
+
+    private fun appendItem() {
+
+    }
+
+    private fun updateItem() {
+
+    }
+
+
 }
