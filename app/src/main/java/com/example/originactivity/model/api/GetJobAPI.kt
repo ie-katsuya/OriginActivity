d06cc3d30f62519cf450284468c2f0ce28f6a5cb
ie-katsuya <k-aoki@i-enter.co.jp>
2019-06-04 17:01:32 +0900

    GetJobAPIを修正中

--- app/src/main/java/com/example/originactivity/model/api/GetJobAPI.kt
+++ app/src/main/java/com/example/originactivity/model/api/GetJobAPI.kt
@@ -1,5 +1,6 @@
 package com.example.originactivity.model.api
 
+import android.util.Log
 import com.example.originactivity.Const
 import com.example.originactivity.model.entity.Job
 import com.google.firebase.database.DataSnapshot
@@ -9,7 +10,6 @@ import com.google.firebase.database.ValueEventListener
 class GetJobAPI : FirebaseAPI() {
 
     fun getJob(taskId: String, callback: (List<Job>) -> Unit) {
-        val uid = user?.uid ?: return
         val contentRef = firebaseReference
             .child(Const.ContentsPATH)
             .child(taskId)
@@ -18,17 +18,31 @@ class GetJobAPI : FirebaseAPI() {
             object : ValueEventListener {
                 override fun onCancelled(p0: DatabaseError) {
                     //未使用
+                    Log.d("test", "みしよう")
                 }
 
                 override fun onDataChange(datasnapshot: DataSnapshot) {
-                    getTaskItem(datasnapshot, callback)
+                    //getJobItem(datasnapshot, callback)
+                    Log.d("test", "しよう")
+                    //関与しているタスクをリストに表示
+                    val jobIdList = mutableListOf<String>()
+                    datasnapshot.children.forEach { item ->
+                        var jobId = item.key ?: return@forEach
+                        jobIdList.add(jobId)
+                    }
+                    val jobList = mutableListOf<Job>()
+
+                    val favoriteTask = datasnapshot.toJob()
+                    jobList.add(favoriteTask)
+                    callback(jobList)
+
                     contentRef.removeEventListener(this)
                 }
             }
         )
     }
 
-    private fun getTaskItem(datasnapshot: DataSnapshot, callback: (List<Job>) -> Unit) {
+    private fun getJobItem(datasnapshot: DataSnapshot, callback: (List<Job>) -> Unit) {
         //関与しているタスクをリストに表示
         val jobIdList = mutableListOf<String>()
         datasnapshot.children.forEach { item ->
@@ -39,6 +53,11 @@ class GetJobAPI : FirebaseAPI() {
         var currentCount = 0L
         val jobList = mutableListOf<Job>()
 
+        val favoriteTask = datasnapshot.toJob()
+        jobList.add(favoriteTask)
+        if (currentCount >= totalCount) {
+            callback(jobList)
+        }
 
     }
 
@@ -46,7 +65,7 @@ class GetJobAPI : FirebaseAPI() {
         val map = this.value as Map<String, Any>
         val title = map.get("title") ?: ""
         val date = map.get("date")?.let { it as? Long } ?: 0L
-        val jobUid = this.key ?: ""
-        return Job(title as String, date, jobUid)
+        val jobId = this.key ?: ""
+        return Job(title as String, date, jobId)
     }
 }
